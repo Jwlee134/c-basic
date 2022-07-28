79774f82933a91297aaa4513fa91db25c52f9550
Jaewon <sorhd134@gmail.com>
2022-07-28 17:49:00 +0900

    loop

---
+++ src/loop.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    /* for (int i = 0; i < 10; i++)
+    {
+        printf("Hello for\n");
+    }
+
+    int i = 0;
+    while (i < 10)
+    {
+        printf("Hello while\n");
+        i++;
+    }
+
+    int j = 0;
+    do
+    {
+        printf("Hello dowhile\n");
+        j++;
+    } while (j < 10); */
+
+    int floor;
+    printf("피라미드는 몇 층인가? ");
+    scanf("%d", &floor);
+
+    for (int i = 0; i < floor; i++)
+    {
+        for (int j = 0; j < floor - i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < i * 2 + 1; k++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
