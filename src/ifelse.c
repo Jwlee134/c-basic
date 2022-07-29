97b8f6e6db97a65b56fcf4f57eda1170a1ac414d
Jaewon <sorhd134@gmail.com>
2022-07-29 17:53:29 +0900

    ifelse

---
+++ src/ifelse.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));             // Initialization, should only be called once.
+    int random = rand() % 100 + 1; // Returns a pseudo-random integer between 0 and RAND_MAX.
+    int chance = 10;
+    int input;
+
+    while (1)
+    {
+        printf("남은 기회는 %d번 입니다. 숫자를 입력하세요: ", chance--);
+        scanf("%d", &input);
+
+        if (chance == 0)
+        {
+            printf("남은 기회가 없습니다. 게임을 종료합니다.\n");
+            break;
+        }
+        if (input == random)
+        {
+            printf("성공! 게임을 종료합니다.\n");
+            break;
+        }
+        else if (input > random)
+        {
+            printf("Down!\n");
+            continue;
+        }
+        else
+        {
+            printf("Up!\n");
+            continue;
+        }
+    }
+
+    return 0;
+}
